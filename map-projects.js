e92d381e074cf2bc60d4176678102a5d9c364bfd
kartikeya-gaur <kartikeyag@iisc.ac.in>
2026-09-07 17:42:54 +0530

    Update map-projects.js for new project reports

--- map-projects.js
+++ map-projects.js
@@ -13,8 +13,8 @@ const map = L.map("map", {
 /* -------------------------
    1. Basemap (No Labels)
 -------------------------- */
-L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png", {
-  attribution: "&copy; OpenStreetMap & CARTO",
+L.tileLayer("https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png", {
+  attribution: "&copy; OpenStreetMap contributors &copy; CARTO",
   subdomains: 'abcd',
   maxZoom: 20
 }).addTo(map);
